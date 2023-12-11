5e1ac9d05b97ab5a9566f55861130b06d46f024f
Amol Salunke <salunkeamol10@gmail.com>
2023-12-11 10:57:18 +0530

    Loops in c++ -> while loop

---
+++ 22_loops1.cpp
@@ -0,0 +1,15 @@
+// Loops in c++ -> while loop
+//code-1 count Down
+#include <iostream>
+using namespace std;
+int main()
+{
+    int countDown = 10;
+    while (countDown > 0)
+    {
+        cout << countDown << endl;
+        countDown--;
+    }
+    cout << "Go........!" << endl;
+    return 0;
+}
