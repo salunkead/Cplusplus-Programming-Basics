44c3da71f158eb3d2d95d7619d23de2b90eeaa32
Amol Salunke <salunkeamol10@gmail.com>
2023-12-11 10:04:14 +0530

    First c++ program

---
+++ 01_firstCppProgram.cpp
@@ -0,0 +1,18 @@
+//First c++ program
+#include <iostream> //header file
+using namespace std;
+int main()
+{
+    cout << "hello,github..!";
+    return 0;
+}
+
+/*
+1.a c++ program is the collection of functions.in above code,there is only one function i.e main
+2.execution of program starts from the main function
+3.every c++ program must have main function.
+4.main function return integer when called.return 0 indicates successful execution of program
+5.cout is used to display string specified in ""(double quote) on our terminal/console
+6.the operator '<<' is called insertion operator or put to operator
+7.this cout object is defined inside iostram header file
+*/
